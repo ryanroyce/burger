a70ed4abd50f99b6ec20fb879d5357698a6861ea
dissonance <ryanminor01u@yahoo.com>
2019-01-25 15:15:11 -0600

    schema/seeds complete

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers(
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    PRIMARY KEY (id),
+    burger_name VARCHAR(255) NOT NULL,
+    devoured BOOLEAN NOT NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+SELECT * FROM burgers;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Whopper', false),
+('Big Mac', false),
+('Double Double', false);
