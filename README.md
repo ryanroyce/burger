cc44a7fc7a65ef97ff5a87854c84c64876de8ee2
dissonance <ryanminor01u@yahoo.com>
2019-02-02 00:49:15 -0600

    jawsDB

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 ## Overview
 This is a burger logger that uses MySQL, Node, Express, Handlebars and a homemade ORM. It follows the MVC design pattern; The app uses Node and MySQL to query and route data in the app, and Handlebars to generate HTML.
-
+<br>
+<br>
+![one](/burger/public/assets/images/one.png)
+<br>
+<br>
 To see the deployed version of this using heroku please go to: https://burger-app-rminor.herokuapp.com/
 
 To see this and other projects go to: https://ryanroyce.github.io/portfolio/
--- config/connection.js
+++ config/connection.js
@@ -3,12 +3,20 @@ const mysql = require("mysql");
 
 // connect to mysql with the template that was used in the Cats activity week 14 activity 17
 const connection = mysql.createConnection({
-  host: "localhost",
+  host: "ls0znzigqvfehvff5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "ryanminor",
-  password: "limabean13",
-  database: "burgers_db"
+  user: "ah2dhhpgbnb4tggu",
+  password: "t0pby8uis0w6qjvz",
+  database: "ujjoz42hcksxfu8y"
 });
+
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "ryanminor",
+//   password: "limabean13",
+//   database: "burgers_db"
+// });
 
 // Make connection.
 connection.connect((err) =>{
